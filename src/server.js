091e8e15706ee552855fedf63f7a5126923936e6
kingseungil <flykim5115@gmail.com>
2023-01-10 10:33:44 +0900

    ws 연결

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -12,6 +14,11 @@ app.get('/', (req, res) => {
 app.get('/*', (req, res) => {
     res.redirect('/');
 });
-app.listen(3000, () => {
-    console.log('서버가 열렸어용');
-});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
