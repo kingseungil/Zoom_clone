de5636b842ecd08f5c20acf809a9e574ee9234a6
kingseungil <flykim5115@gmail.com>
2023-01-10 09:52:36 +0900

    프론트 틀 완성

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/*', (req, res) => {
+    res.redirect('/');
+});
 app.listen(3000, () => {
     console.log('서버가 열렸어용');
 });
